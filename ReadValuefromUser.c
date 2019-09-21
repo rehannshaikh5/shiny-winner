af0283163f6da194de7eb53cff05bb2389a44582
rehannshaikh5 <55617756+rehannshaikh5@users.noreply.github.com>
2019-09-21 15:06:29 +0530

    Add files via upload

---
+++ ReadValuefromUser.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,sum;
+    printf("Enter the first value:");
+    scanf("%d",&a);
+    printf("\nEnter the second value:");
+    scanf("%d",&b);
+    sum= a+b;
+    printf("\nThe sum is: %d",sum);
+}
