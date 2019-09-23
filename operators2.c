c07574fef526f6f5035d693545132c9c4bfa2433
rehannshaikh5 <55617756+rehannshaikh5@users.noreply.github.com>
2019-09-23 21:26:44 +0530

    Add files via upload

---
+++ operators2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=6; int b=3;
+printf("%d AND %d= %d ",a,b,a&b);
+printf("%d OR %d=%d",a,b,a|b);
+printf("%d XOR %d=%d",a,b,a^b);
+getch();
+
+}
